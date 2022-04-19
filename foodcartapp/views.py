02c66b03e3869581ac06f0dad9e5f89b595fed06
clownkill <alehnovich90@yandex.ru>
2022-04-19 15:33:50 +0300

    Extend data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,3 +1,4 @@
+import phonenumbers
 from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework.decorators import api_view
@@ -65,18 +66,31 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     order = request.data
-
+    order_fields = ['products', 'firstname', 'lastname', 'phonenumber', 'address']
     try:
-        order['products']
+        for field in order_fields:
+            order[field]
     except KeyError:
-        content = {'products': 'Обязательное поле'}
+        content = {'products, firstname, lastname, phonenumber, address': 'Обязательное поле'}
         return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
     if isinstance(order['products'], str):
         content = {'products': 'Поле должно содержать list со значениями. Был получен str'}
         return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
-    if not order['products']:
-        content = {'products': 'Поле не может быть пустым'}
+    if not isinstance(order['firstname'], str):
+        content = {'firstname': f'Поле должно содержать str. Был получен {type(order["firstname"])}'}
         return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
+    phonenumber = phonenumbers.parse(order['phonenumber'])
+    if not phonenumbers.is_valid_number(phonenumber):
+        content = {'phonenumber': 'Введен некорректный номер телефона'}
+        return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
+    try:
+        content = {}
+        for field in order_fields:
+            if not order[field]:
+                content.update({field: 'Поле не может быть пустым'})
+        return Response(content, status=status.HTTP_406_NOT_ACCEPTABLE)
+    except:
+        pass
 
     customer = Order.objects.create(
         first_name=order['firstname'],
